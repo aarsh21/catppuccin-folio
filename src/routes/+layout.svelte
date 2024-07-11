8f8e4a3b44af6211bd4b7d8f6d027cf841991d6c
aarsh21 <aarshpersonal21@gmail.com>
2024-07-11 18:15:20 +0530

    chore: add other routes for navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<main class="mx-auto h-full w-[50%]">
+<main class=" relative mx-auto mt-8 h-full w-[50%] bg-ctp-crust px-2.5 text-ctp-text antialiased">
+	<Navbar />
 	<slot></slot>
 </main>
---
+++ src/routes/guestbook/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="text-ctp-peach">WIP</h1>
