6222065c73e8ce8ca74ddeebfeca3210910f12ea
aarsh21 <aarshpersonal21@gmail.com>
2024-07-12 09:21:15 +0530

    fix: styling on the root layout and navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	const styles =
-		'text-ctp-peach underline decoration-wavy decoration-[1.5px] underline-offset-[6px]';
+		'text-ctp-peach underline decoration-wavy decoration-[1.5px] underline-offset-[6px] ';
 </script>
 
 <aside class="tracking-light mb-8">
 	<nav class="flex scroll-pr-6 flex-row items-start gap-x-4 px-0 py-0 text-2xl font-bold">
-		<a href="/" class={$page.route.id === '/' ? `${styles}` : ''}>home</a>
-		<a href="/blog" class={$page.route.id === '/blog' ? `${styles}` : ''}>blog</a>
-		<a href="/guestbook" class={$page.route.id === '/guestbook' ? `${styles}` : ''}>guestbook</a>
+		<a href="/" class={$page.route.id === '/' ? `${styles}` : 'hover:text-ctp-overlay1'}>home</a>
+		<a href="/blog" class={$page.route.id === '/blog' ? `${styles}` : 'hover:text-ctp-overlay1'}
+			>blog</a
+		>
+		<a
+			href="/guestbook"
+			class={$page.route.id === '/guestbook' ? `${styles}` : 'hover:text-ctp-overlay1'}>guestbook</a
+		>
 	</nav>
 </aside>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,9 @@
 	import Navbar from '../components/Navbar.svelte';
 </script>
 
-<main class=" relative mx-auto mt-8 h-full w-[50%] bg-ctp-crust px-2.5 text-ctp-text antialiased">
+<main
+	class=" relative mx-auto mt-8 h-full w-full min-w-[320px] max-w-[1000px] text-balance bg-ctp-crust px-2.5 text-ctp-text antialiased sm:w-[90%]"
+>
 	<Navbar />
 	<slot></slot>
 </main>
