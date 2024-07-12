84096119ecf2e2a7182dd9c521967b3245297587
aarsh21 <aarshpersonal21@gmail.com>
2024-07-12 13:29:07 +0530

    chore:add description on home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,7 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<main
-	class=" relative mx-auto mt-8 h-full w-full min-w-[320px] max-w-[1000px] text-balance bg-ctp-crust px-2.5 text-ctp-text antialiased sm:w-[90%]"
->
+<main class=" bg-ctp-crust px-2.5 text-lg text-ctp-text antialiased">
 	<Navbar />
 	<slot></slot>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
 	import Avatar from '$lib/components/Avatar.svelte';
+	import Icon from '@iconify/svelte';
 </script>
 
 <div class="mt-4 flex items-center justify-self-auto text-3xl font-semibold">
 	<Avatar />
 	<h1 class="ml-4">Aarsh padia</h1>
 </div>
+<div class="ml-[6px] mt-4 flex items-center text-lg">
+	<Icon icon="mingcute:location-2-line" class="h-6 w-6" />
+	<h1 class="ml-2">Rajkot , Gujarat</h1>
+</div>
+<div class="ml-[6px] mt-2 flex items-center text-lg">
+	<Icon icon="uil:suitcase-alt" class="h-6 w-6" />
+	<h1 class="ml-2">OSS contributor and Web Developer</h1>
+</div>
+
+<p class="mt-4">
+	I'm a 20 y/o cs undergrad student. I love building things , playing games and ricing my Arch
+	Linux. I enjoy web development and i live on the terminal. If I'm not coding, I'm probably losing
+	my mind in valorant or watching anime.
+</p>
