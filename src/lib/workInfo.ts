a0594242b93e228b7312a727bd80633e492d52ef
aarsh21 <aarshpersonal21@gmail.com>
2024-07-17 17:57:22 +0530

    chore : add data about work and projects

---
+++ src/lib/workInfo.ts
@@ -0,0 +1,60 @@
+export const Work = [
+	{
+		position: 'Web Development Intern',
+		company: 'Dcycle Design Studio',
+		start_date: '06/2023',
+		end_date: '08/2023',
+		responsibilities: [
+			'Elevated organizational productivity by designing and implementing HTML/CSS templates for webpages.',
+			'Optimized user experience through enhancements in the visual appeal and functionality of web content.',
+			'Streamlined development processes, contributing to a 50% increase in efficiency for the design team.'
+		]
+	},
+	{
+		position: 'Web Development Intern',
+		company: 'Dcycle Design Studio',
+		start_date: '06/2023',
+		end_date: '08/2023',
+		responsibilities: [
+			'Elevated organizational productivity by designing and implementing HTML/CSS templates for webpages.',
+			'Optimized user experience through enhancements in the visual appeal and functionality of web content.',
+			'Streamlined development processes, contributing to a 50% increase in efficiency for the design team.'
+		]
+	}
+];
+
+export const Projects = [
+	{
+		project: 'Chat-App',
+		details: [
+			'Gained expertise in building full-stack applications using Next.js 14, enhancing understanding of server-side rendering and modern front-end development practices.',
+			'Developed strong skills in database management and optimization by integrating Neon DB with Prisma, learning to efficiently handle complex queries and data relationships.',
+			'Mastered the implementation of real-time communication features using LiveKit, improving knowledge of socket connections and real-time data synchronization.',
+			'Implemented secure authentication and authorization using Clerk, reinforcing understanding of user identity management and application security.'
+		]
+	},
+	{
+		project: 'Ematchi',
+		details: [
+			'Gained expertise in building interactive user interfaces using Svelte, improving understanding of reactive programming and efficient component-based architecture.',
+			'Developed strong skills in designing game mechanics and logic, creating an engaging emoji matching game with three distinct modes, enhancing problem-solving abilities.',
+			'Learned to manage complex state and improve user experience by creating intuitive and responsive UI elements tailored for a seamless gaming experience.',
+			'Applied best practices for web performance optimization, ensuring fast load times and smooth gameplay across different devices and browsers.'
+		]
+	},
+	{
+		project: 'Article Digester',
+		details: [
+			'Article Digester is a web application that allows users to summarize articles by providing the summarized text from the URL.',
+			'Utilizes Redux Toolkit, Vite with React, and Tailwind CSS to deliver a seamless and intuitive user experience.',
+			'Utilized Rapid API to seamlessly process and extract relevant information from articles, showcasing proficiency in integrating external services to enhance content digestion on a professional platform.'
+		]
+	},
+	{
+		project: 'Slime-Link',
+		details: [
+			'Created Slime Link, an open-source alternative to Tree Link, using Svelte for dynamic user interfaces and Firebase for efficient backend management.',
+			'Focused on responsive design, ensuring seamless usability across devices and enhancing user accessibility for efficient link management.'
+		]
+	}
+];
