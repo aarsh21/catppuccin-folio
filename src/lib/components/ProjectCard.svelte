12a9a1d6e4bca92ce6cab144de98a7e54a1cc0b4
aarsh21 <aarshpersonal21@gmail.com>
2024-07-17 17:58:10 +0530

    chore: fix card component to the following data

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
+	export let position: string,
+		company: string,
+		start_date: string,
+		end_date: string,
+		responsiblities: string[];
 </script>
 
-<Card.Root class="sm:col-span-2">
-	<Card.Header class="pb-3">
-		<Card.Title>Your Orders</Card.Title>
-		<Card.Description class="max-w-lg text-balance leading-relaxed">
-			Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.
+<Card.Root class="mt-6 sm:col-span-2">
+	<Card.Header class="pb-6">
+		<Card.Title class="w-full">{company}</Card.Title>
+		<h1 class="mb-2 text-lg font-semibold">{position}</h1>
+		<Card.Description
+			class=" lg:text-md mt-0 text-sm leading-tight lg:text-nowrap lg:leading-relaxed"
+		>
+			{#each responsiblities as response}
+				<li>{response}</li>
+			{/each}
 		</Card.Description>
 	</Card.Header>
-	<Card.Footer>
-		<Button>Create New Order</Button>
-	</Card.Footer>
 </Card.Root>
--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-	import type { HTMLAttributes } from "svelte/elements";
-	import { cn } from "$lib/utils.js";
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = HTMLAttributes<HTMLDivElement>;
 
-	let className: $$Props["class"] = undefined;
+	let className: $$Props['class'] = undefined;
 	export { className as class };
 </script>
 
 <div
-	class={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+	class={cn(
+		'mx-auto max-h-96  rounded-lg border border-ctp-peach bg-ctp-crust text-ctp-text shadow-sm',
+		className
+	)}
 	{...$$restProps}
 >
 	<slot />
