8146828cb3fcf57d1e6862dd34755279628da12e
aarsh21 <aarshpersonal21@gmail.com>
2024-07-17 17:57:39 +0530

    chore : add accent color to icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import Avatar from '$lib/components/Avatar.svelte';
 	import ProjectCard from '$lib/components/ProjectCard.svelte';
 	import Icon from '@iconify/svelte';
+	import { Work } from '$lib/workInfo';
 </script>
 
 <div class="mt-4 flex items-center justify-self-auto text-3xl font-semibold">
@@ -9,22 +10,27 @@
 	<h1 class="ml-4">Aarsh padia</h1>
 </div>
 <div class="ml-[6px] mt-4 flex items-center text-lg">
-	<Icon icon="mingcute:location-2-line" class="h-6 w-6" />
+	<Icon icon="mingcute:location-2-line" class="h-6 w-6 text-ctp-peach" />
 	<h1 class="ml-2">Rajkot , Gujarat</h1>
 </div>
 <div class="ml-[6px] mt-2 flex items-center text-lg">
-	<Icon icon="uil:suitcase-alt" class="h-6 w-6" />
+	<Icon icon="uil:suitcase-alt" class=" h-6 w-6 text-ctp-peach" />
 	<h1 class="ml-2">OSS contributor and Web Developer</h1>
 </div>
-
-<p class="mt-4">
+<p class="mb-2 mt-4">
 	I'm a 20 y/o cs undergrad student. I love building things , playing games and ricing my Arch
 	Linux. I enjoy web development and i live on the terminal. If I'm not coding, I'm probably losing
 	my mind in valorant or watching anime.
 </p>
-
-<div class="mt-4">
-	<h1 class="text-3xl font-semibold">
-		<ProjectCard />
-	</h1>
+<div>
+	<h1 class="mb-4 text-3xl font-semibold">Work</h1>
+	{#each Work as work}
+		<ProjectCard
+			position={work.position}
+			company={work.company}
+			end_date={work.end_date}
+			start_date={work.start_date}
+			responsiblities={work.responsibilities}
+		/>
+	{/each}
 </div>
