e4125d42d7af4f896a96425326f7a70f1e19769a
aarsh21 <aarshpersonal21@gmail.com>
2024-07-18 11:44:33 +0530

    chore: add project card component

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,22 +1,14 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
 	import * as Card from '$lib/components/ui/card';
-	export let position: string,
-		company: string,
-		start_date: string,
-		end_date: string,
-		responsiblities: string[];
+	export let project: string, details: string[];
 </script>
 
-<Card.Root class="mt-6 sm:col-span-2">
+<Card.Root class="col-span-1 mt-6 hover:border-ctp-sky sm:col-span-2">
 	<Card.Header class="pb-6">
-		<Card.Title class="w-full">{company}</Card.Title>
-		<h1 class="mb-2 text-lg font-semibold">{position}</h1>
-		<Card.Description
-			class=" lg:text-md mt-0 text-sm leading-tight lg:text-nowrap lg:leading-relaxed"
-		>
-			{#each responsiblities as response}
-				<li>{response}</li>
+		<Card.Title>{project}</Card.Title>
+		<Card.Description class=" mt-0 ">
+			{#each details as detail}
+				<li class=" text-[1rem] leading-tight">{detail}</li>
 			{/each}
 		</Card.Description>
 	</Card.Header>
