43e05020bd8008d26fc6779447aefabc2f5519b9
aarsh21 <aarshpersonal21@gmail.com>
2024-07-25 20:44:36 +0530

    chore: abstract some more code from +page.svelte

---
+++ src/lib/components/Main.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import WorkCard from '$lib/components/WorkCard.svelte';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import { Projects, Work } from '$lib/profileDetails';
+</script>
+
+<div>
+	<h1 class="mb-4 text-3xl font-semibold">Work</h1>
+	{#each Work as work}
+		<WorkCard
+			position={work.position}
+			company={work.company}
+			end_date={work.end_date}
+			start_date={work.start_date}
+			responsiblities={work.responsibilities}
+		/>
+	{/each}
+</div>
+<div>
+	<h1 class="mb-4 mt-6 text-3xl font-semibold">Projects</h1>
+	{#each Projects as project}
+		<a href={project.link}>
+			<ProjectCard project={project.project} details={project.details} />
+		</a>
+	{/each}
+</div>
