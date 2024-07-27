eb4e688bec92366780f8ed0f38f55c09182c6e36
aarsh21 <aarshpersonal21@gmail.com>
2024-07-27 21:39:16 +0530

    fix: styles on navbar squiggly fixed

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,17 +2,22 @@
 	import { page } from '$app/stores';
 	const styles =
 		'text-ctp-peach underline decoration-wavy decoration-[1.5px] underline-offset-[6px] ';
+	console.log($page.route.id);
 </script>
 
 <aside class="tracking-light mb-8">
 	<nav class="flex scroll-pr-6 flex-row items-start gap-x-4 px-0 py-0 text-2xl font-bold">
-		<a href="/" class={$page.route.id === '/' ? `${styles}` : 'hover:text-ctp-overlay1'}>home</a>
-		<a href="/blog" class={$page.route.id === '/blog' ? `${styles}` : 'hover:text-ctp-overlay1'}
-			>blog</a
+		<a href="/" class={$page.route.id === '/(app)' ? `${styles}` : 'hover:text-ctp-overlay1'}
+			>home</a
+		>
+		<a
+			href="/blog"
+			class={$page.route.id === '/(app)/blog' ? `${styles}` : 'hover:text-ctp-overlay1'}>blog</a
 		>
 		<a
 			href="/guestbook"
-			class={$page.route.id === '/guestbook' ? `${styles}` : 'hover:text-ctp-overlay1'}>guestbook</a
+			class={$page.route.id === '/(app)/guestbook' ? `${styles}` : 'hover:text-ctp-overlay1'}
+			>guestbook</a
 		>
 	</nav>
 </aside>
