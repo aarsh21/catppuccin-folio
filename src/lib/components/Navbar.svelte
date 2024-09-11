2c1a2242072dada09fa51775dccf477dc4619a9f
aarsh21 <aarshpersonal21@gmail.com>
2024-09-11 10:15:31 +0530

    fix: some styling on mobile view

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,7 +4,7 @@
 		'text-cat_primary underline decoration-cat_quaternary decoration-wavy decoration-[1.7px] underline-offset-[7px] ';
 </script>
 
-<aside class="tracking-light mb-8 mt-10">
+<aside class="tracking-light mb-8 mt-8">
 	<nav class="flex scroll-pr-6 flex-row items-start gap-x-4 px-0 py-0 text-xl font-bold">
 		<a href="/" class={$page.route.id === '/(app)' ? `${styles}` : 'hover:text-cat_quaternary'}
 			>home</a
